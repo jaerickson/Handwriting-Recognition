1c7e7f10cfd42d9f80f25521b04fe82b0d83277f
DoctorEtc <100000551+DoctorEtc@users.noreply.github.com>
2022-04-22 14:14:59 -0700

    Update handwriting_recognition.py

--- handwriting_recognition.py
+++ handwriting_recognition.py
@@ -1,8 +1,9 @@
 #Imports
 import pandas as pd
 
-#Creates a variable df that holds data from answers.csv
-df = pd.read_csv ('answers.csv')
+#Creates a variable "answers" that holds data from answers.csv
+answers = pd.read_csv ('answers.csv')
 
+answers = answers[~answers['BallotID'].isin(nonexistent)]
 #Print data as a test
-print(df)
+print(answers)
