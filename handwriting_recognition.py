d4f58402b29382b623e9485c22b1f7822b15729f
DoctorEtc <100000551+DoctorEtc@users.noreply.github.com>
2022-04-22 14:11:39 -0700

    Update handwriting_recognition.py

--- handwriting_recognition.py
+++ handwriting_recognition.py
@@ -1,2 +1,8 @@
-# start of new files changed
+#Imports
+import pandas as pd
 
+#Creates a variable df that holds data from answers.csv
+df = pd.read_csv ('answers.csv')
+
+#Print data as a test
+print(df)
