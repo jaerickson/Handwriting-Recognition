f1aa6ac9da18dcad6174516e3728dc3cd0cc032d
DoctorEtc <100000551+DoctorEtc@users.noreply.github.com>
2022-04-25 14:30:25 -0700

    Update handwriting_recognition.py

--- handwriting_recognition.py
+++ handwriting_recognition.py
@@ -4,8 +4,9 @@ import sklearn as sk
 
 answers_raw_data = pd.read_csv('answers.csv')
 
-#Training set = 60%
+#Training set = 80%
 #Testing Set = 20%
-#Validation Set = 20%
+
+#Validation Set = 20% of remaining training set
 train_set, test_set = train_test_split(answers,test_size=0.2,train_size=0.8)
-train_set, validation_set = train_test_split(train_set,test_size=0.25,train_size=0.75)
+train_set, validation_set = train_test_split(train_set,test_size=0.2,train_size=0.8)
