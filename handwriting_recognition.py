6e417f84329f267e8a68560fb52c8f1f5d2f142b
DoctorEtc <100000551+DoctorEtc@users.noreply.github.com>
2022-04-25 14:24:53 -0700

    Update handwriting_recognition.py

--- handwriting_recognition.py
+++ handwriting_recognition.py
@@ -7,4 +7,5 @@ answers_raw_data = pd.read_csv('answers.csv')
 #Training set = 60%
 #Testing Set = 20%
 #Validation Set = 20%
-train_set, test_set, validation_set = train_test_split(answers_raw_data,test_size=0.2,train_size=0.6,validation_size=0.2)
+train_set, test_set = train_test_split(answers,test_size=0.2,train_size=0.8)
+train_set, validation_set = train_test_split(train_set,test_size=0.25,train_size=0.75)
