c3a91c3b69cd9260ee1d9c994b288da015c3a548
Jessica Erickson <jessicaerickson@lclark.edu>
2022-04-26 20:24:13 -0700

    Milestones 6 & 7 as well as other commented lines ignore

--- handwriting_recognition.py
+++ handwriting_recognition.py
@@ -1,16 +1,17 @@
-#Imports
+# Imports
 from os.path import exists
 import keras as keras
 import pandas as pd
 import numpy as np
 import sklearn as sk
-import sklearn.model_selection as skmodel
-from sklearn.model_selection import train_test_split
+import sklearn.model_selection as sk_model
 from PIL import Image
+import tensorflow as tf
 
 # Milestone 1:
 # Read answers.csv into a pandas dataframe. It should have a length of 47703.
-answers_raw_data = pd.read_csv('answers.csv')
+# answers_raw_data = pd.read_csv('answers.csv')
+answers_raw_data = pd.read_csv('answers.csv', nrows=1000)
 print(len(answers_raw_data))
 
 # Milestone 2:
@@ -21,34 +22,68 @@ for _, x in file_list:
     fileName = x["BallotID"]
     filePath = "writein_crops/" + fileName + ".jpg"
     file_exists = exists(filePath)
-    if file_exists == False:
-        missing_files+=[fileName]
+    if not file_exists:
+        missing_files += [fileName]
 answers_cleaned = answers_raw_data[~answers_raw_data["BallotID"].isin(missing_files)]
 print(len(answers_cleaned))
 
 # Milestone 3:
-# Read all of the images into a single, three-dimensional numpy array (img_array).
-img_array = np.empty(len(answers_cleaned),53,358)
+# Read all the images into a single, three-dimensional numpy array (img_array).
+img_array = np.empty((len(answers_cleaned), 53, 358))
 file_list = answers_cleaned.iterrows()
 counter = 0
 for _, x in file_list:
     fileName = x["BallotID"]
     img = np.array(Image.open("writein_crops/" + fileName + ".jpg"))
-    img_array[counter,:,:] = img[:53,:358] / 255
-    counter+=1
+    img_array[counter, :, :] = img[:53, :358] / 255
+    counter += 1
 
 # Milestone 4 & 5:
 # Set aside 20% of the data for testing.
 # Set aside 20% of the remaining data for validation.
-#trainSetX & testSetX & validationSetX = array of images
-#trainSetY & testSetY & validationSetY = 0 or 1 in Raiford column of answers.csv
-trainSetX, testSetX, trainSetY, testSetY = train_test_split(img_array,answers_cleaned["raiford"],test_size=0.2,train_size=0.8)
-trainSetX, validationSetX, trainSetY, validationSetY = train_test_split(trainSetX,trainSetY,test_size=0.2,train_size=0.8)
+# trainSetX & testSetX & validationSetX = array of images
+# trainSetY & testSetY & validationSetY = 0 or 1 in Raiford column of answers.csv
+trainSetX, testSetX, trainSetY, testSetY = sk_model.train_test_split(img_array, answers_cleaned["raiford"],
+                                                                     test_size=0.2, train_size=0.8)
+trainSetX, validationSetX, trainSetY, validationSetY = sk_model.train_test_split(trainSetX, trainSetY,
+                                                                                 test_size=0.2, train_size=0.8)
+# Milestone 6 & 7
+model = keras.models.Sequential([
+    keras.layers.Flatten(),
+    keras.Input(shape=(53, 358)),
+    keras.layers.Dense(32, activation='sigmoid'),
+])
+model.compile(
+    loss='sparse_categorical_crossentropy',
+    optimizer='adam',
+    metrics='accuracy'
+)
+model.fit(
+    x=trainSetX,
+    y=trainSetY,
+    epochs=10,
+    validation_data=(validationSetX, validationSetY)
+)
+model.summary()
 
-#NOTES for next steps: USE Tensorflow Keras (big machine learning library)
-#Likely in textbook
-#Start by defining model
-model=keras.models.sequential()
-#Start with dense layer with sigmoid function
-#For compile model.compile
-#For fitting model model.fit (needs inputs)
+
+
+
+
+
+
+# history = model.fit(x=trainSetX, y=trainSetY, epochs=10, validation_data=(validationSetX, validationSetY))
+
+# print(model.output_shape)
+# print(model.compute_output_signature)
+
+# inputs = tf.keras.Input(shape=(3,))
+# x = tf.keras.layers.Dense(4, activation=tf.nn.relu)(inputs)
+# outputs = tf.keras.layers.Dense(5, activation=tf.nn.softmax)(x)
+# model = tf.keras.Model(inputs=inputs, outputs=outputs)
+
+# Start with dense layer with sigmoid function
+# For compile model.compile
+# For fitting model: model.fit (needs inputs)
+
+
